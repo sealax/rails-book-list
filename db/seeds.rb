a14977ee9ad6771c4e3f1576a5ffa197df34589c
Chris Sealey <csealey1@gmail.com>
2026-05-13 11:53:49 +0100

    Test db seeded from Open Library api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,39 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "open-uri"
+require "json"
+
+puts "Cleaining database..."
+
+Bookmark.destroy_all
+Book.destroy_all
+List.destroy_all
+
+puts "Fetching sci-fi books..."
+
+url = "https://openlibrary.org/search.json?q=science+fiction&limit=20"
+
+serialized_books = URI.open(url).read
+books = JSON.parse(serialized_books)
+
+books["docs"].each do |book_data|
+  next if book_data["title"].nil?
+  next if book_data["author_name"].nil?
+  next if book_data["cover_i"].nil?
+
+  Book.create!(
+    title: book_data["title"],
+    author: book_data["author_name"].first,
+    description: "First published in #{book_data["first_publish_year"]|| "unknown year"}",
+    cover_url: "https://covers.lopenlibrary.org/b/id/#{book_data["cover_i"]}-L.jpg"
+  )
+end
+
+puts "#{Book.count} sci-fi books created!"
+
+puts "Creating lists..."
+
+List.create!(name: "Space Operas")
+List.create!(name: "Cyberpunk")
+List.create!(name: "AI and Robots")
+List.create!(name: "Classics")
+
+puts "Done!"
