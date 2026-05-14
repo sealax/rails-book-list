f975f426ffe15f75654c033706f149a49408923e
Chris Sealey <csealey1@gmail.com>
2026-05-14 12:08:42 +0100

    Improved book adding functionality

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,45 @@
+
+
+class BookmarksController < ApplicationController
+  require "open-uri"
+  require "json"
+  def new
+    @list = List.find(params[:list_id])
+
+    if params[:query].present?
+      url = "http://openlibrary.org/search.json?q=#{URI.encode_www_form_component(params[:query])}&limit=10"
+
+      response = URI.open(url).read
+      data = JSON.parse(response)
+
+      @results = data["docs"]
+    else
+      @results = []
+    end
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+
+    @book = Book.find_or_create_by(open_library_key: params[:open_library_key]) do |book|
+      book.title = params[:title]
+      book.author = params[:author]
+      book.cover_url = params[:cover_url]
+      book.description = "Added from Open Library"
+    end
+
+  @bookmark = Bookmark.new(
+    list: @list,
+    book: @book,
+    comment: params[:comment],
+    rating: params[:rating]
+  )
+
+  if @bookmark.save
+    redirect_to list_path(@list)
+  else
+    @results = []
+    render :new, status: :unprocessable_entity
+  end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "bookmarks/new"
+  get "bookmarks/create"
+  get "bookmarks/destroy"
   root to: "lists#index"
-  resources :lists, only: [ :index, :show, :new, :create ]
+  resources :lists, only: [ :index, :show, :new, :create, :destroy ] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+
+  resources :bookmarks, only: [ :destroy ]
 end
